ebf4a180f1c18159264a01b1cfd68774ea775f30
Kevin Muscara <muscarak1@nku.edu>
2023-05-22 22:31:06 -0400

    request builder using superagent

---
+++ src/web/http.js
@@ -0,0 +1,51 @@
+const superagent = require('superagent'); 
+let HttpManager = {};
+
+let _getParametersFromRequest = function(request) {
+  var options = {};
+
+  if(request.getQueryParameters()) {
+    options.query = request.getQueryParameters();
+  }
+
+  if(request.getHeaders() && request.getHeaders()['Content-Type'] === 'application/json') {
+    options.data = JSON.stringify(request.getBodyParameters());
+  } else if(request.getBodyParameters()) {
+    options.data = request.getBodyParameters();
+  }
+
+  if(request.getHeaders()) {
+    options.headers = request.getHeaders();
+  }
+
+  return options;
+}
+
+HttpManager._makeRequest = function(method, options, uri, callback) {
+  let req = method.bind(superagent)(uri);
+
+  if(options.query) req.query(options.query);
+  if (options.headers) req.set(options.headers);
+  if (options.data) req.send(options.data);
+  
+  req.end(function(err, response) {
+    if(err) {
+      return callback(err);
+    }
+
+    return callback(null, {
+      body: response.body,
+      headers: response.headers,
+      statusCode: response.statusCode
+    });
+  });
+};
+
+HttpManager.get = function(request, callback) {
+  var options = _getParametersFromRequest(request);
+  var method = superagent.get;
+
+  HttpManager._makeRequest(method, options, request.getURI(), callback);
+};
+
+module.exports = HttpManager;
